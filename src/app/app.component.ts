e6953323b033699941e4b79953c976618035079b
Carlos Sánchez <holapueblodev@gmail.com>
2023-05-09 19:55:26 +0200

    inicio proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Matrix_Frontend_Angular';
+
+  changeTitle(){
+    this.title = 'change';
+  }
+}
